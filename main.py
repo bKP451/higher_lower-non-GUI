f3e939c70ce74588dec9514d24ebc04dd1a02876
bkp451 <bkppandey87@gmail.com>
2022-03-20 23:23:53 +0545

    working optimized model

--- main.py
+++ main.py
@@ -3,40 +3,49 @@ from game_art import logo, vs
 import random
 import os
 
-
+user_score = 0
 def clear(): os.system('clear')
 
 
-user_score = 0
 game_finished = False
-temp_dict = {}
-first_option = random.choice(data)
-temp_dict = first_option
 
+
+def statements(entry):
+    name = entry["name"]
+    des = entry["description"]
+    country = entry["country"]
+    return f"{name}, a {des}, from {country}"
+
+
+def compare(first_option, second_option, user_guess):
+    # I can use boolean here
+    if first_option['follower_count'] < second_option['follower_count']:
+        return user_guess == 'b'
+    else:
+        return user_guess == 'a'
+
+
+second_entry = random.choice(data)
 while not game_finished:
-    clear()
-    print(logo)
-    print(f"Compare A: {temp_dict['name']}, a {temp_dict['description']}, from {temp_dict['country']}  ")
-    print(vs)
-    second_option = random.choice(data)
-    print(f"Against B: {second_option['name']}, a {second_option['description']}, from {second_option['country']}  ")
-    user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
-    if user_choice == 'a':
-        if temp_dict['follower_count'] > second_option['follower_count']:
-            user_score = user_score + 1
-            print(f"You are right. Current score {user_score}")
-            temp_dict = second_option
-        else:
-            print(f"Sorry that's wrong. Final score : {user_score}")
-            game_finished = True
-    elif user_choice == 'b':
-        if second_option['follower_count'] > temp_dict['follower_count']:
-            user_score = user_score + 1
-            print(f"You are right. Current score {user_score}")
-            temp_dict = second_option
-        else:
-            print(f"Sorry that's wrong. Final score : {user_score}")
-            game_finished = True
 
+    # Here I always have to compare the second_entry to the newly generated random entry
+    first_entry = second_entry
+    second_entry = random.choice(data)
+    # first and  second entry should be unique
+    if first_entry == second_entry:
+        second_entry = random.choice(data)
+    print(logo)
+    print(f"Compare A: {statements(first_entry)}")
+    print(vs)
+    print(f"Against B: {statements(second_entry)}")
+
+    guess = input("Who has more followers ? Type 'A' or 'B' ").lower()
+    clear()
+    if compare(first_entry, second_entry, guess):
+        user_score = user_score + 1
+        print(f"You guessed right !! Your score is {user_score}")
+    else:
+        print(f"Sorry you guessed it wrong. Score is {user_score}")
+        game_finished = True
 
 
